0bca1363d687cb9839dd5458cbaaa5b55ea7a9b2
Curtis D'Alves <curtis.dalves@gmail.com>
2020-01-25 19:29:55 -0500

    added hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Hello Moodule
+
+def print_hello():
+    """ simple function that does nothing but print Hello """
+    print("Hello")
+
+def print_world():
+    """ simple function that does nothing but print World """
+    print("World")
