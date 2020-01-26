05f578277be6590cff4c2fe6dd9a62cac218202a
Curtis D'Alves <curtis.dalves@gmail.com>
2020-01-25 19:44:55 -0500

    added print_goodbye function to hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,10 @@ def print_hello():
     """ simple function that does nothing but print Hello """
     print("Hello")
 
+def print_goodbye():
+    """ simple function that does nothing but print GoodBye """
+    print("GoodBye")
+
 def print_world():
     """ simple function that does nothing but print World """
     print("World")
