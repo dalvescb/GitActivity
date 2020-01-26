c172d0c8a37e404c9770ecbe3bb0635d3d82fb7c
Curtis D'Alves <curtis.dalves@gmail.com>
2020-01-25 19:53:13 -0500

    added print_friend function to hello.py

--- hello.py
+++ hello.py
@@ -7,3 +7,7 @@ def print_hello():
 def print_world():
     """ simple function that does nothing but print World """
     print("World")
+
+def print_friend():
+    """ simple function that does nothing but print Friend """
+    print("Friend")
